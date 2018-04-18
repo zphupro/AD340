fc03dadb9ce6c0c9041f893b8d042e02de4410af
zphupro <38090459+zphupro@users.noreply.github.com>
2018-04-18 11:23:45 -0700

    Initial commit

---
+++ app/src/main/java/biz/zphu/activity_first/MainActivity.java
@@ -0,0 +1,34 @@
+package biz.zphu.activity_first;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText textBox;
+    Button passButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textBox = (EditText)findViewById(R.id.textBox);
+        passButton = (Button)findViewById(R.id.passButton);
+
+        passButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = textBox.getText().toString();
+
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                intent.putExtra("message", str);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
