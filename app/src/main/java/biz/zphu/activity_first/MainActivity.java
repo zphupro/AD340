cf748caa74d98247475b9306a9a71c5ba05481e5
zphupro <38090459+zphupro@users.noreply.github.com>
2018-04-26 17:18:47 -0700

    Hw3 half way done

--- app/src/main/java/biz/zphu/activity_first/MainActivity.java
+++ app/src/main/java/biz/zphu/activity_first/MainActivity.java
@@ -7,28 +7,41 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-    EditText textBox;
-    Button passButton;
+        public class MainActivity extends AppCompatActivity {
+            EditText textBox;
+            Button passButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        textBox = (EditText)findViewById(R.id.textBox);
-        passButton = (Button)findViewById(R.id.passButton);
-
-        passButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String str = textBox.getText().toString();
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
 
-                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
-                intent.putExtra("message", str);
 
+
+//                textBox = (EditText)findViewById(R.id.textBox);
+//                passButton = (Button)findViewById(R.id.passButton);
+//
+//                passButton.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        String str = textBox.getText().toString();
+//
+//                        Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+//                        intent.putExtra("message", str);
+//
+//                        startActivity(intent);
+//                    }
+//                });
+
+
+
+
+
+    }
+
+
+    public void movieinfo(View view){
+                Intent intent = new Intent(this, Movieitem.class);
                 startActivity(intent);
-            }
-        });
     }
 }
