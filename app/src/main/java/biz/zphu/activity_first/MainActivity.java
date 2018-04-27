b8a53c01df7ad2febe68c2734897476fced28f93
zphupro <38090459+zphupro@users.noreply.github.com>
2018-04-26 19:21:04 -0700

    add toast to button

--- app/src/main/java/biz/zphu/activity_first/MainActivity.java
+++ app/src/main/java/biz/zphu/activity_first/MainActivity.java
@@ -1,21 +1,21 @@
 package biz.zphu.activity_first;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-        public class MainActivity extends AppCompatActivity {
-            EditText textBox;
-            Button passButton;
+public class MainActivity extends AppCompatActivity {
+    EditText textBox;
+    Button passButton;
 
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
 
 //                textBox = (EditText)findViewById(R.id.textBox);
@@ -34,14 +34,32 @@ import android.widget.EditText;
 //                });
 
 
+    }
+
+
+    public void movieinfo(View view) {
+        Intent intent = new Intent(this, Movieitem.class);
+        startActivity(intent);
 
 
 
     }
 
 
-    public void movieinfo(View view){
-                Intent intent = new Intent(this, Movieitem.class);
-                startActivity(intent);
+    public void button2(View v){
+
+        Toast.makeText(getApplicationContext(),"Hello", Toast.LENGTH_SHORT).show();
     }
+
+    public void fun(View v){
+
+        Toast.makeText(getApplicationContext(),"Fun", Toast.LENGTH_SHORT).show();
+    }
+
+    public void more(View v){
+
+        Toast.makeText(getApplicationContext(),"More", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
