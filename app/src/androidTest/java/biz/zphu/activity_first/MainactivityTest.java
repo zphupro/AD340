b59034d871c3f5b99eb099d3ff1f7985ab7d5c37
zphupro <38090459+zphupro@users.noreply.github.com>
2018-05-10 21:22:23 -0500

    add test to android test for hw5

--- app/src/androidTest/java/biz/zphu/activity_first/MainactivityTest.java
+++ app/src/androidTest/java/biz/zphu/activity_first/MainactivityTest.java
@@ -1,19 +1,33 @@
 package biz.zphu.activity_first;
 
-import android.app.Activity;
+
 import android.app.Instrumentation;
-import android.content.Context;
-import android.content.Intent;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
-
-
+import android.content.Context;
+import android.preference.PreferenceManager;
+import android.content.Intent;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+
+import static org.junit.Assert.*;
+
+import android.view.View;
+import android.widget.EditText;
+import android.content.SharedPreferences;
 
 
 
 
 
 import android.widget.EditText;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 
 
 public class MainactivityTest {
@@ -24,18 +38,59 @@ public class MainactivityTest {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class, true,
             false); // Activity is not launched immediately
 
-    private MainActivity mActivity = null;
-    private String mField = " ";
+    private MainActivity Maact;
+    private EditText editText1;
     private Intent intent;
-    private String xInput = "T";
-   // private SharedPreferences.Editor preferencesEditor;
+    private SharedPreferences.Editor preedit;
     private EditText editTextBoxEntry;
 
+    Instrumentation.ActivityMonitor aMonitor = getInstrumentation().addMonitor(Main2Activity.class.getName(), null, false);
+
+    public MainactivityTest() {
+        Maact = null;
+    }
 
 
+    @Before
+    public void setUp() throws Exception {
+        Maact = mActivityTestRule.getActivity();
 
+        intent = new Intent();
+        Context context = getInstrumentation().getTargetContext();
 
-        }
+        // create a SharedPreferences editor
+        preedit = PreferenceManager.getDefaultSharedPreferences(context).edit();
+
+        // create an Edit Text box for use by main activity
+        editTextBoxEntry = (EditText) Maact.findViewById(biz.zphu.activity_first.R.id.textBox2);
 
     }
+    @Test
+    public void testViewswilldisplaywhenenter(){
+        View buttonView = Maact.findViewById(R.id.passButton9);
+        View editTextView = Maact.findViewById(R.id.textBox2);
+        assertNotNull(editTextView); assertNotNull(buttonView);
+    }
+
+
+
+    @Test
+    public void onCreate() {
+    }
+
+
+    @Test
+    public void onCreateOptionsMenu() {
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        Maact = null;
+    }
+
+
+
+
 }
+
