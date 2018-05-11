01d8ad66a3b41fb9003244a08b82cb2b3a7b45ac
zphupro <38090459+zphupro@users.noreply.github.com>
2018-05-11 00:30:10 -0500

    compltete test for android hw5

--- app/src/androidTest/java/biz/zphu/activity_first/MainactivityTest.java
+++ app/src/androidTest/java/biz/zphu/activity_first/MainactivityTest.java
@@ -1,56 +1,43 @@
 package biz.zphu.activity_first;
-
-
+import android.app.Activity;
 import android.app.Instrumentation;
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.content.Context;
 import android.preference.PreferenceManager;
 import android.content.Intent;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
-
-import static org.junit.Assert.*;
-
 import android.view.View;
 import android.widget.EditText;
 import android.content.SharedPreferences;
 
-
-
-
-
-import android.widget.EditText;
-
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
-
-
 public class MainactivityTest {
+
+    private MainActivity Maact;
+    private EditText textBox2;
+    private Intent intent;
+    private SharedPreferences.Editor preferencesEditor;
+
+    private SharedPreferences.Editor preedit;
+    private EditText editTextBoxEntry;
+    Instrumentation.ActivityMonitor Monitor = getInstrumentation().addMonitor(Main2Activity.class.getName(), null, false);
+
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class, true,
-            false); // Activity is not launched immediately
-
-    private MainActivity Maact;
-    private EditText editText1;
-    private Intent intent;
-    private SharedPreferences.Editor preedit;
-    private EditText editTextBoxEntry;
-
-    Instrumentation.ActivityMonitor aMonitor = getInstrumentation().addMonitor(Main2Activity.class.getName(), null, false);
-
-    public MainactivityTest() {
-        Maact = null;
-    }
-
-
+            false);
     @Before
     public void setUp() throws Exception {
         Maact = mActivityTestRule.getActivity();
@@ -66,14 +53,51 @@ public class MainactivityTest {
 
     }
     @Test
+    // this will test when user enter..
     public void testViewswilldisplaywhenenter(){
         View buttonView = Maact.findViewById(R.id.passButton9);
         View editTextView = Maact.findViewById(R.id.textBox2);
         assertNotNull(editTextView); assertNotNull(buttonView);
     }
+    @Test
+    //this will test when click button go
+    public void TestopenOfSendButtonClick() {
 
+        onView(withId(R.id.passButton9)).perform(click());
 
+        Activity secondActivity = getInstrumentation().waitForMonitorWithTimeout(Monitor, 5000);
 
+        assertNotNull(secondActivity);
+
+    }
+    @Test
+    public void testValueUserInput() {
+        editTextBoxEntry.clearComposingText();
+        String testUsername = "test"; preedit.putString("username", testUsername); preedit.commit();
+    }
+
+    @Test
+    // this will test when user doent enter anything
+    public void testEditTextForEmptyInput() {
+
+        assertFalse(" ".isEmpty());
+        onView(withId(R.id.textBox2)).perform(clearText()); onView(withId(R.id.passButton9)).perform(click());
+    }
+
+    // Tests for data storage & retrieval
+    @Test
+    // this will test for storage and retrieval when
+    public void populateUsernameFromSharedPrefsTest() {
+        String testUsername = "test";
+        // Set SharedPreferences data
+        preedit.putString("username", testUsername);
+        preedit.commit();
+        // Launch activity
+        mActivityRule.launchActivity(intent);
+        onView(withId(R.id.textBox2))
+                .check(matches(isDisplayed()));
+
+    }
     @Test
     public void onCreate() {
     }
